2c6cbb5b8b4a0dc7592d6ba2ddb9778c974884fb
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-10-26 17:48:33 +0100

    api working

--- my-shop/src/App.js
+++ my-shop/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Shop</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
